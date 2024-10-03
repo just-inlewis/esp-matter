ca15624932c899b81f4cae3474990d8aa8e3f82a
just-inlewis <98793314+just-inlewis@users.noreply.github.com>
2024-10-02 21:18:55 -0700

    Update app_priv.h

--- examples/light/main/app_priv.h
+++ examples/light/main/app_priv.h
@@ -15,23 +15,12 @@
 #include "esp_openthread_types.h"
 #endif
 
-/** Standard max values (used for remapping attributes) */
-#define STANDARD_BRIGHTNESS 255
-#define STANDARD_HUE 360
-#define STANDARD_SATURATION 255
-#define STANDARD_TEMPERATURE_FACTOR 1000000
-
 /** Matter max values (used for remapping attributes) */
 #define MATTER_BRIGHTNESS 254
-#define MATTER_HUE 254
-#define MATTER_SATURATION 254
-#define MATTER_TEMPERATURE_FACTOR 1000000
 
 /** Default attribute values used during initialization */
 #define DEFAULT_POWER true
 #define DEFAULT_BRIGHTNESS 64
-#define DEFAULT_HUE 128
-#define DEFAULT_SATURATION 254
 
 typedef void *app_driver_handle_t;
 
