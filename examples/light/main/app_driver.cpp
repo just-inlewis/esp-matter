58d3bfd71420b06fa2c70800e8cef116b28f6415
just-inlewis <98793314+just-inlewis@users.noreply.github.com>
2024-10-02 21:19:08 -0700

    Update app_driver.cpp

--- examples/light/main/app_driver.cpp
+++ examples/light/main/app_driver.cpp
@@ -1,14 +1,8 @@
-/*
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <esp_log.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "driver/i2c_master.h"
 
 #include <esp_matter.h>
 #include "bsp/esp-bsp.h"
@@ -21,86 +15,78 @@ using namespace esp_matter;
 static const char *TAG = "app_driver";
 extern uint16_t light_endpoint_id;
 
-/* Do any conversions/remapping for the actual value here */
-static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
-{
-#if CONFIG_BSP_LEDS_NUM > 0
-    esp_err_t err = ESP_OK;
-    if (val->val.b) {
-        err = led_indicator_start(handle, BSP_LED_ON);
-    } else {
-        err = led_indicator_start(handle, BSP_LED_OFF);
+#define I2C_MASTER_NUM I2C_NUM_0
+#define I2C_MASTER_SCL_IO 22
+#define I2C_MASTER_SDA_IO 21
+#define I2C_MASTER_FREQ_HZ 100000
+#define RELAY_ADDR 0x21
+#define MUTE 0x00
+#define MAX_VOLUME 63
+
+esp_err_t i2c_master_init() {
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    conf.clk_flags = 0;
+    esp_err_t err = i2c_param_config(I2C_MASTER_NUM, &conf);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "I2C param config failed: %s", esp_err_to_name(err));
+        return err;
+    }
+    err = i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "I2C driver install failed: %s", esp_err_to_name(err));
     }
     return err;
-#else
-    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
-    return ESP_OK;
-#endif
+}
+
+esp_err_t i2c_write(uint8_t data) {
+    ESP_LOGI(TAG, "Writing to hardware: %d", data);
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (RELAY_ADDR << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, data, true);
+    i2c_master_stop(cmd);
+    esp_err_t err = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return err;
+}
+
+esp_err_t ra_write(unsigned short data) {
+    esp_err_t err = ESP_OK;
+    err |= i2c_write(0x3f); // Write 0x3F to avoid noise and sleep 600ms
+    vTaskDelay(pdMS_TO_TICKS(1));
+    err |= i2c_write(data);
+    return err;
+}
+
+static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    esp_err_t err = ESP_OK;
+    if (!val->val.b) {
+        ESP_LOGI(TAG, "Muting device");
+        err |= ra_write(MUTE);
+    }
+    return err;
 }
 
 static esp_err_t app_driver_light_set_brightness(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
 {
-    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, STANDARD_BRIGHTNESS);
-#if CONFIG_BSP_LEDS_NUM > 0
-    return led_indicator_set_brightness(handle, value);
-#else
-    ESP_LOGI(TAG, "LED set brightness: %d", value);
-    return ESP_OK;
-#endif
-}
-
-static esp_err_t app_driver_light_set_hue(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
-{
-    int value = REMAP_TO_RANGE(val->val.u8, MATTER_HUE, STANDARD_HUE);
-#if CONFIG_BSP_LEDS_NUM > 0
-    led_indicator_ihsv_t hsv;
-    hsv.value = led_indicator_get_hsv(handle);
-    hsv.h = value;
-    return led_indicator_set_hsv(handle, hsv.value);
-#else
-    ESP_LOGI(TAG, "LED set hue: %d", value);
-    return ESP_OK;
-#endif
-}
-
-static esp_err_t app_driver_light_set_saturation(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
-{
-    int value = REMAP_TO_RANGE(val->val.u8, MATTER_SATURATION, STANDARD_SATURATION);
-#if CONFIG_BSP_LEDS_NUM > 0
-    led_indicator_ihsv_t hsv;
-    hsv.value = led_indicator_get_hsv(handle);
-    hsv.s = value;
-    return led_indicator_set_hsv(handle, hsv.value);
-#else
-    ESP_LOGI(TAG, "LED set saturation: %d", value);
-    return ESP_OK;
-#endif
-}
-
-static esp_err_t app_driver_light_set_temperature(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
-{
-    uint32_t value = REMAP_TO_RANGE_INVERSE(val->val.u16, STANDARD_TEMPERATURE_FACTOR);
-#if CONFIG_BSP_LEDS_NUM > 0
-    return led_indicator_set_color_temperature(handle, value);
-#else
-    ESP_LOGI(TAG, "LED set temperature: %ld", value);
-    return ESP_OK;
-#endif
-}
-
-static void app_driver_button_toggle_cb(void *arg, void *data)
-{
-    ESP_LOGI(TAG, "Toggle button pressed");
-    uint16_t endpoint_id = light_endpoint_id;
-    uint32_t cluster_id = OnOff::Id;
-    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
-
-    attribute_t *attribute = attribute::get(endpoint_id, cluster_id, attribute_id);
-
-    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
-    attribute::get_val(attribute, &val);
-    val.val.b = !val.val.b;
-    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+    esp_err_t err = ESP_OK;
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, MAX_VOLUME);
+    ESP_LOGI(TAG, "Setting volume: %d", value);
+    if (value > MAX_VOLUME) {
+        ESP_LOGE(TAG, "Invalid volume level");
+        return ESP_ERR_INVALID_ARG;
+    }
+    unsigned short data = (~value) | 0x40;
+    ESP_LOGI(TAG, "Setting volume: %d, data: 0x%02X", value, data);
+    err |= ra_write(data);
+    return err;
 }
 
 esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
@@ -116,14 +102,6 @@ esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_
         } else if (cluster_id == LevelControl::Id) {
             if (attribute_id == LevelControl::Attributes::CurrentLevel::Id) {
                 err = app_driver_light_set_brightness(handle, val);
-            }
-        } else if (cluster_id == ColorControl::Id) {
-            if (attribute_id == ColorControl::Attributes::CurrentHue::Id) {
-                err = app_driver_light_set_hue(handle, val);
-            } else if (attribute_id == ColorControl::Attributes::CurrentSaturation::Id) {
-                err = app_driver_light_set_saturation(handle, val);
-            } else if (attribute_id == ColorControl::Attributes::ColorTemperatureMireds::Id) {
-                err = app_driver_light_set_temperature(handle, val);
             }
         }
     }
@@ -142,27 +120,6 @@ esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
     attribute::get_val(attribute, &val);
     err |= app_driver_light_set_brightness(handle, &val);
 
-    /* Setting color */
-    attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::ColorMode::Id);
-    attribute::get_val(attribute, &val);
-    if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kCurrentHueAndCurrentSaturation) {
-        /* Setting hue */
-        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::CurrentHue::Id);
-        attribute::get_val(attribute, &val);
-        err |= app_driver_light_set_hue(handle, &val);
-        /* Setting saturation */
-        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::CurrentSaturation::Id);
-        attribute::get_val(attribute, &val);
-        err |= app_driver_light_set_saturation(handle, &val);
-    } else if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kColorTemperature) {
-        /* Setting temperature */
-        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::ColorTemperatureMireds::Id);
-        attribute::get_val(attribute, &val);
-        err |= app_driver_light_set_temperature(handle, &val);
-    } else {
-        ESP_LOGE(TAG, "Color mode not supported");
-    }
-
     /* Setting power */
     attribute = attribute::get(endpoint_id, OnOff::Id, OnOff::Attributes::OnOff::Id);
     attribute::get_val(attribute, &val);
@@ -173,24 +130,6 @@ esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
 
 app_driver_handle_t app_driver_light_init()
 {
-#if CONFIG_BSP_LEDS_NUM > 0
-    /* Initialize led */
-    led_indicator_handle_t leds[CONFIG_BSP_LEDS_NUM];
-    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, CONFIG_BSP_LEDS_NUM));
-    led_indicator_set_hsv(leds[0], SET_HSV(DEFAULT_HUE, DEFAULT_SATURATION, DEFAULT_BRIGHTNESS));
-    
-    return (app_driver_handle_t)leds[0];
-#else
+    ESP_ERROR_CHECK(i2c_master_init());
     return NULL;
-#endif
-}
-
-app_driver_handle_t app_driver_button_init()
-{
-    /* Initialize button */
-    button_handle_t btns[BSP_BUTTON_NUM];
-    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
-    ESP_ERROR_CHECK(iot_button_register_cb(btns[0], BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL));
-    
-    return (app_driver_handle_t)btns[0];
 }
